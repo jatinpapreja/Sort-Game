bdc53cd7aca9eeed51d061eb0a9cbfd8ab2c7e53
jatinpapreja <47968703+jatinpapreja@users.noreply.github.com>
2020-01-21 16:58:57 +0530

    Add files via upload

---
+++ Sort Game.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+bool mycompare(pair<string,int>p1,pair<string,int>p2)
+{
+  if(p1.second==p2.second)
+  {
+    return p1.first<p2.first;
+  }
+  return p1.second>p2.second;
+}
+int main() {
+  int n,x;
+  cin>>x;
+  cin>>n;
+  pair<string,int>emp[100005];
+  string name;
+  int salary;
+  for(int i=0;i<n;i++)
+  {
+    cin>>name>>salary;
+    emp[i].first=name;
+    emp[i].second=salary;
+  }
+  sort(emp,emp+n,mycompare);
+  for(int i=0;i<n;i++)
+  {
+    if(emp[i].second>=x)
+    {
+      cout<<emp[i].first<<" "<<emp[i].second<<endl;
+    }
+  }
+	return 0;
+}
